d1fa1a585c9711075b5f42de5bcfcb84ccf1d0a8
Shawn Inder <shawn@vigour.io>
2016-08-10 16:09:25 -0400

    fixes for new tests

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /**
  * Expose `isUrl`.
  */
@@ -6,18 +5,27 @@
 module.exports = isUrl;
 
 /**
- * Matcher.
+ * Matchers.
  */
 
-var matcher = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/;
+var protocol = '[a-z]\\:\\/\\/|(\\/\\/)';
+var domain = '[a-zA-Z@:0-9_-]+';
+var other = '[a-zA-Z/0-9$-/:-?{#-~!"^_`\\[\\]]+';
+var ext = '\\.' + other;
+var port = '\\:[0-9]+';
+var ip = '([a-zA-Z0-9]{4}:)+[a-zA-Z0-9]';
 
 /**
  * Loosely validate a URL `string`.
- *
  * @param {String} string
+ * @param {Object} [options] - defaults to `{}`
+ * - **options.requireProtocol** {Boolean} - set to true if you only want URLs with a protocol to be considered valid. Defaults to `false`
  * @return {Boolean}
  */
-
-function isUrl(string){
-  return matcher.test(string);
+function isUrl (string, opts) {
+  opts = opts || {}
+  var reStr = '^' + protocol + ((opts.requireProtocol) ? '' : '?') + '(' + domain + ext + '|localhost|' + ip + ')' + '(' + port + ')?' + '(' + other + ')' + '?$'
+  // console.log('reStr', reStr)
+  var re = new RegExp(reStr)
+  return typeof string === 'string' && re.test(string)
 }
